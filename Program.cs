ac8cdd5289c3f24828d3d447964a9ae5bfefef9f
Matheus Sousa <matheusss.dev01@gmail.com>
2025-05-21 01:45:22 -0300

    Limpando o Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using Workspace.Entities;
 using Workspace.Entities.Enums;
 
@@ -8,20 +9,7 @@ namespace Workspace
     {
         public static void Main(string[] args)
         {
-            Order order = new Order()
-            {
-                Id = 1800,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
 
-
-            string txt = OrderStatus.PendingPayment.ToString();
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-
-            Console.WriteLine(order);
-            Console.WriteLine(txt);
-            Console.WriteLine(os);
         }
     }
 }
