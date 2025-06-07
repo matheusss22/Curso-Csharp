66f585ea730ae3f1e789ffdb5536a405b8a5787b
Matheus Sousa <matheusss.dev01@gmail.com>
2025-06-07 16:40:17 -0300

    Introdução a Exceptions

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Globalization;
-using Workspace.Entities;
 
 namespace Workspace;
 
@@ -8,48 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<TaxPayer> taxPayers = [];
-
-        Console.Write("Enter the number of tax payers: ");
-        int n = int.Parse(Console.ReadLine());
-
-        for (int i = 1; i <= n; i++)
+        try
         {
-            Console.WriteLine($"Tax payer #{i} data:");
-            Console.Write($"Individual or company(i/c)? ");
-            char op = char.Parse(Console.ReadLine());
-
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Anual income: ");
-            double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            if (op == 'i')
-            {
-                Console.Write("Health expenditures: ");
-                double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                taxPayers.Add(new Individual(name, anualIncome, healthExpenditures));
-            }
-            else if (op == 'c')
-            {
-                Console.Write("Number of employees: ");
-                int numberOfEmployees = int.Parse(Console.ReadLine());
-                taxPayers.Add(new Company(name, anualIncome, numberOfEmployees));
-            }
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int result = n1 / n2;
+            Console.WriteLine(result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Division by zero is not allwed");
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Format error! " + e.Message);
         }
 
-        Console.WriteLine("\nTAXES PAID:");
-
-        double sum = 0;
-
-        foreach (var taxPayer in taxPayers)
-        {
-            Console.WriteLine($"{taxPayer.Name}: ${taxPayer.Tax().ToString("F2", CultureInfo.InvariantCulture)}");
-            sum += taxPayer.Tax();
-        }
-
-        Console.WriteLine($"\nTOTAL TAXES: $ {sum.ToString("F2", CultureInfo.InvariantCulture)}");
     }
 }
-
-
